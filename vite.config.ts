9296b811239b8b6e690d56bcc3f0fe9c51551b46
prajwald2607 <smartpd2607@gmail.com>
2025-04-19 16:00:35 +0530

    integrated automated deployment

--- vite.config.ts
+++ vite.config.ts
@@ -1,10 +1,7 @@
 import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react";
-// https://vite.dev/config/
+
 export default defineConfig({
-  base: "/Vaishali-hair-oil/",
+  base: "/Vaishali-hair-oil/", 
   plugins: [react()],
-  build: {
-    outDir: "docs" 
-  }
 });
